8506dd90e62d0d57c9e7a9aba7745b606dfc7ff7
zzzhxxx <fiberbell@163.com>
2021-08-24 19:34:17 +0800

    first commit

---
+++ one_way_calendar_api.php
@@ -0,0 +1,6 @@
+<?php
+    $a = date("Y/md");
+    $txt = "http://img.owspace.com/Public/uploads/Download/$a.jpg";
+    header('Content-type: image/jpeg');
+    echo file_get_contents(isset($_GET["url"])?$txt:'');
+?>
